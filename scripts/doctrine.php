1e8f214903a583a76dd139068775f82f4abb334b
Jeremias Nunez <jereman_gnr@hotmail.com>
2012-03-07 03:47:24 -0200

    Adding README and fixed library path error in Doctrine CLI tool

--- scripts/doctrine.php
+++ scripts/doctrine.php
@@ -9,9 +9,9 @@ defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
 
 
-// Ensure library/ is on include_path
+// Ensure vendor/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
+    realpath(APPLICATION_PATH . '/../vendor'),
     get_include_path(),
 )));
 
